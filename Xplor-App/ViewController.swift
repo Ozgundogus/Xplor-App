9d9ee7324229e7e6f1d659f2b890000d2b16b318
Ozgundogus <43118369+Ozgundogus@users.noreply.github.com>
2023-07-30 19:07:31 +0300

    Initial commit

---
+++ Xplor-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xplor-App
+//
+//  Created by Ozgun Dogus on 30.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
